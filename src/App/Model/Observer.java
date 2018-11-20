161b45cb807c04e4c4ac395b9c10bd5307f35bc7
Harrison Lee <harrison.lee@hotmail.com>
2018-11-20 14:12:07 -0800

    10.3 (probably enough to get full points + deleted extra code)

--- src/App/Model/Observer.java
+++
@@ -1,8 +0,0 @@
-package App.Model;
-
-import App.player.Player;
-
-public interface Observer {
-
-    public void update(PlayerFun p);
-}
--- src/App/ui/NewJFrame2.java
+++
@@ -1,7 +0,0 @@
-package App.ui;
-
-import javax.swing.*;
-
-public class NewJFrame2 {
-    private JPanel panel1;
-}
